2abc97e779b359f8e056e2e9c4e716df9ec8dffc
Antonia Blair <antoniablair@gmail.com>
2016-06-19 16:31:03 -0400

    Begin creating calculatorBrain model

--- kittyCalculator/calculatorBrain.swift
+++ kittyCalculator/calculatorBrain.swift
@@ -1,5 +1,5 @@
 //
-//  calculatorBrain.swift
+//  CalculatorBrain.swift
 //  kittyCalculator
 //
 //  Created by Antonia Blair on 6/18/16.
@@ -8,9 +8,22 @@
 
 import Foundation
 
-class calculatorBrain {
+class CalculatorBrain {
+    // API for CalculatorBrain
+    func setOperand(operand: Double) { }
+    
+    func performOperation(symbol: String) { }
+    
+    var result: Double {
+        // This is a read-only property (doesn't have set)
+        get {
+            // TODO: change return 0.0 to return the answer
+            return 0.0
+        }
+    }
     
     init() {
         print ("calculatorBrain is initialized")
+        
     }
 }
